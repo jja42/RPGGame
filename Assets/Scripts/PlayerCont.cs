7c107d9f7ec1a5be96b69bb3e8fa379d444ae0ab
Joao Amaral <jja42@njit.edu>
2019-09-27 21:52:26 -0400

    Ink test script working and Tilemap working along with simple animations

---
+++ Assets/Scripts/PlayerCont.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCont : MonoBehaviour
+{
+    Direction currentDir;
+    Vector2 input;
+    bool isMoving = false;
+    Vector3 startPos;
+    Vector3 endPos;
+    float t;
+
+    public Sprite northSprite;
+    public Sprite eastSprite;
+    public Sprite southSprite;
+    public Sprite westSprite;
+    public Animator animator;
+    public float walkSpeed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator  = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isMoving)
+        {
+            input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+            {
+                input.y = 0;
+            }
+            else
+            {
+                input.x = 0;
+            }
+            if (input != Vector2.zero)
+            {
+                if (input.x < 0)
+                {
+                    currentDir = Direction.West;
+                    animator.SetFloat("Horizontal", input.x);
+                    animator.SetFloat("Vertical", input.y);
+                } else if (input.x > 0)
+                {
+                    currentDir = Direction.East;
+                    animator.SetFloat("Horizontal", input.x);
+                    animator.SetFloat("Vertical", input.y);
+                   
+                }else if (input.y < 0)
+                {
+                    currentDir = Direction.South;
+                    animator.SetFloat("Horizontal", input.x);
+                    animator.SetFloat("Vertical", input.y);
+                    
+                }else if (input.y > 0)
+                {
+                    currentDir = Direction.North;
+                    animator.SetFloat("Horizontal", input.x);
+                    animator.SetFloat("Vertical", input.y);
+                   
+                }
+
+                StartCoroutine(Move(transform));
+            }
+            else
+            {
+                animator.SetFloat("Horizontal", input.x);
+                animator.SetFloat("Vertical", input.y);
+            }
+        }
+    }
+    public IEnumerator Move(Transform entity)
+    {
+        isMoving = true;
+        startPos = entity.position;
+        t = 0;
+        endPos = new Vector3(startPos.x + System.Math.Sign(input.x), startPos.y + System.Math.Sign(input.y), startPos.z);
+        while (t < 1f)
+        {
+            t += Time.deltaTime * walkSpeed;
+            entity.position = Vector3.Lerp(startPos, endPos, t);
+            yield return 0;
+        }
+        isMoving = false;
+        yield return 0;
+    }
+    enum Direction
+    {
+        North,
+        East,
+        South,
+        West
+    }
+}
